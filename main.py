e84c70d4dd9d295c80db855e1a207274dfc7cb64
Ihor Mykytiuk <igor14vn@gmail.com>
2025-04-07 01:08:54 +0300

    docker configuration

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ api = Api(app)
 
 class HelloWorld(Resource):
     def get(self):
-        return {'hello': 'world'}
+        return {'hello': 'word'}
 
 
 api.add_resource(HelloWorld, '/')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", debug=True)
